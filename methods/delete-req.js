69efcde0c075b85ebc724bb2d40623b8890143a7
GouthamGajam <gouthamgajam@gmail.com>
2024-06-30 23:13:17 +0530

    DAYY 5

--- methods/delete-req.js
+++ methods/delete-req.js
@@ -21,5 +21,8 @@ module.exports = (req, res) => {
             res.writeHead(204, {"Content-Type": "application/json"});
             res.end(JSON.stringify(req.movies));
         }
+    }else{
+        res.writeHead(404, {"Content-Type":"application/json"});
+        res.end(JSON.stringify({title:"Not Found", message:"Route not found"}));
     }
 };
